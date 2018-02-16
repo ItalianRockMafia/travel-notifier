6a7ee5cb85099a11c6d1fd75eff6d1301b099e74
jhuesser <github@jhuesser.ch>
2018-02-16 16:15:32 +0100

    list events

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 	<head>
 		<meta charset="utf-8">
  	   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-			<link rel="stylesheet" href="global/main.css">
+			<link rel="stylesheet" href="../global/main.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<title>IRM - Meetup planer</title>
 	</head>
@@ -52,8 +52,15 @@ $tg_user = getTelegramUserData();
 
 if ($tg_user !== false) {
 
-
-
+$events = json_decode(getCall($config->api_url . "eventUsers?transform=1"), true);
+?>
+<div class="list-group">
+<?php
+foreach($events['eventUsers'] as $event){
+	echo '<a href="#" class="list-group-item list-group-item-action">' . $event["event_title"] . '</a>';
+	
+}
+?></div><?php
 } else {
 	echo '
 	<div class="alert alert-danger" role="alert">
