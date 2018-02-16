63ca71a234a6d2486b3178acf4901cf5a9abf9c1
jhuesser <github@jhuesser.ch>
2018-02-16 15:31:45 +0100

    grundgerüst

--- index.php
+++ index.php
@@ -1,157 +1,68 @@
+<?php
+session_start();
+?>
 <!doctype html>
 <html>
 	<head>
+		<meta charset="utf-8">
+ 	   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			<link rel="stylesheet" href="global/main.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<title>IRM - Meetup planer</title>
 	</head>
 	<body>
 
-	<?php
-	$config =	require('config.php');
-		require 'functions/apicalls.php';
 
-		$users = json_decode(getCall($config->apiUrl . "userStation?transform=1"), true);
+	<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+	<a class="navbar-brand" href="#">ItalianRockMafia</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+		<li class="nav-item">
+        				<a class="nav-link" href="../main.php">Home</a>
+      				</li>
+			  <li class="nav-item">
+				<a class="nav-link" href="../settings.php">Settings</a>
+			  </li>
+			  <li class="nav-item active">
+				<a class="nav-link" href="#">Meetup<span class="sr-only">(current)</span></a>
+			  </li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+				<li class="nav-item">
+        			<a class="nav-link" href="login.php?logout=1">Logout</a>
+      			</li>
+		</ul>
+	</div>
+</nav>
+<div class="topspacer"></div>
+<main role="main">
+	<div class="container">
 
-		foreach($users["userStation"] as $userdata){
-		$response = json_decode(getCall("http://transport.opendata.ch/v1/connections?from="  . $userdata["station"] . "&to=Baden"));
+<?php
 
-		if ($response->from) {
-			$from = $response->from->name;
-		}
-		if ($response->to) {
-			$to = $response->to->name;
-		}
-		if (isset($response->stations->from[0])) {
-			if ($response->stations->from[0]->score < 101) {
-				foreach (array_slice($response->stations->from, 1, 3) as $station) {
-					if ($station->score > 97) {
-						$stationsFrom[] = $station->name;
-					}
-				}
-			}
-		}
-		if (isset($response->stations->to[0])) {
-			if ($response->stations->to[0]->score < 101) {
-				foreach (array_slice($response->stations->to, 1, 3) as $station) {
-					if ($station->score > 97) {
-						$stationsTo[] = $station->name;
-					}
-				}
-			}
-		}
+require '../global/functions/apicalls.php';
+require '../global/functions/telegram.php';
+$config = require "../config.php";
 
-		foreach ($response->connections as $connection): ?>
-		<?php $j++; ?>
-		<tbody>
-			<tr class="connection"<?php if ($j == $c): ?> style="display: none;"<?php endif; ?> data-c="<?php echo $j; ?>">
-				<td>
-					<?php echo date('H:i', strtotime($connection->from->departure)); ?>
-					<?php if ($connection->from->delay): ?>
-						<span style="color: #a20d0d;"><?php echo '+'.$connection->from->delay; ?></span>
-					<?php endif; ?>
-					<br/>
-					<?php echo date('H:i', strtotime($connection->to->arrival)); ?>
-					<?php if ($connection->to->delay): ?>
-						<span style="color: #a20d0d;"><?php echo '+'.$connection->to->delay; ?></span>
-					<?php endif; ?>
-				</td>
-				<td>
-					<?php echo (substr($connection->duration, 0, 2) > 0) ? htmlentities(trim(substr($connection->duration, 0, 2), '0')).'d ' : ''; ?>
-					<?php echo htmlentities(trim(substr($connection->duration, 3, 1), '0').substr($connection->duration, 4, 4)); ?>′<br/>
-					<span class="muted">
-					<?php echo htmlentities(implode(', ', $connection->products)); ?>
-					</span>
-				</td>
-				<td>
-					<?php if ($connection->from->prognosis->platform): ?>
-						<span style="color: #a20d0d;"><?php echo htmlentities($connection->from->prognosis->platform, ENT_QUOTES, 'UTF-8'); ?></span>
-					<?php else: ?>
-						<?php echo htmlentities($connection->from->platform, ENT_QUOTES, 'UTF-8'); ?>
-					<?php endif; ?>
-					<br/>
-					<?php if ($connection->capacity2nd > 0): ?>
-						<small title="Expected occupancy 2nd class">
-							<?php for ($i = 0; $i < 3; $i++): ?>
-								<?php if ($i < $connection->capacity2nd): ?>
-									<span class="glyphicon glyphicon-user text-muted"></span>
-								<?php else: ?>
-									<span class="glyphicon glyphicon-user text-disabled"></span>
-								<?php endif; ?>
-							<?php endfor; ?>
-						</small>
-					<?php endif; ?>
-				</td>
-			</tr>
-			<?php $i = 0; foreach ($connection->sections as $section): ?>
-				<tr class="section"<?php if ($j != $c): ?> style="display: none;"<?php endif; ?>>
-					<td rowspan="2">
-						<?php echo date('H:i', strtotime($section->departure->departure)); ?>
-						<?php if ($section->departure->delay): ?>
-							<span style="color: #a20d0d;"><?php echo '+'.$section->departure->delay; ?></span>
-						<?php endif; ?>
-					</td>
-					<td>
-						<?php echo htmlentities($section->departure->station->name, ENT_QUOTES, 'UTF-8'); ?>
-					</td>
-					<td>
-						<?php if ($section->departure->prognosis->platform): ?>
-							<span style="color: #a20d0d;"><?php echo htmlentities($section->departure->prognosis->platform, ENT_QUOTES, 'UTF-8'); ?></span>
-						<?php else: ?>
-							<?php echo htmlentities($section->departure->platform, ENT_QUOTES, 'UTF-8'); ?>
-						<?php endif; ?>
-					</td>
-				</tr>
-				<tr class="section"<?php if ($j != $c): ?> style="display: none;"<?php endif; ?>>
-					<td style="border-top: 0; padding: 4px 8px;">
-						<span class="muted">
-						<?php if ($section->journey): ?>
-							<?php echo htmlentities($section->journey->name, ENT_QUOTES, 'UTF-8'); ?>
-						<?php else: ?>
-							Walk
-						<?php endif; ?>
-						</span>
-					</td>
-					<td style="border-top: 0; padding: 4px 8px;">
-						<small title="Expected occupancy 2nd class">
-							<?php if ($section->journey && $section->journey->capacity2nd > 0): ?>
-								<?php for ($i = 0; $i < 3; $i++): ?>
-									<?php if ($i < $section->journey->capacity2nd): ?>
-										<span class="glyphicon glyphicon-user text-muted"></span>
-									<?php else: ?>
-										<span class="glyphicon glyphicon-user text-disabled"></span>
-									<?php endif; ?>
-								<?php endfor; ?>
-							<?php endif; ?>
-						</small>
-					</td>
-				</tr>
-				<tr class="section"<?php if ($j != $c): ?> style="display: none;"<?php endif; ?>>
-					<td style="border-top: 0;">
-						<?php echo date('H:i', strtotime($section->arrival->arrival)); ?>
-						<?php if ($section->arrival->delay): ?>
-							<span style="color: #a20d0d;"><?php echo '+'.$section->arrival->delay; ?></span>
-						<?php endif; ?>
-					</td>
-					<td style="border-top: 0;">
-						<?php echo htmlentities($section->arrival->station->name, ENT_QUOTES, 'UTF-8'); ?>
-					</td>
-					<td style="border-top: 0;">
-						<?php if ($section->arrival->prognosis->platform): ?>
-							<span style="color: #a20d0d;"><?php echo htmlentities($section->arrival->prognosis->platform, ENT_QUOTES, 'UTF-8'); ?></span>
-						<?php else: ?>
-							<?php echo htmlentities($section->arrival->platform, ENT_QUOTES, 'UTF-8'); ?>
-						<?php endif; ?>
-					</td>
-				</tr>
-			<?php endforeach; ?>
-		</tbody>
-	<?php endforeach; ?>
-</table>
 
-	
-	<?php	}?>
-		
+$tg_user = getTelegramUserData();
 
-		
-	
+if ($tg_user !== false) {
 
+
+
+} else {
+	echo '
+	<div class="alert alert-danger" role="alert">
+	<strong>Error.</strong> You need to login first
+  </div>
+';
+}
+?>
+			</div>
+		</main>
 	</body>
 </html>
