1570dd6666220a407ec05e60794bbba9baaf3c83
jhuesser <github@jhuesser.ch>
2018-02-16 16:20:04 +0100

    added event.php

---
+++ event.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+ 	   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			<link rel="stylesheet" href="../global/main.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<script src="https://use.fontawesome.com/c414fc2c21.js"></script>
+		<title>IRM - Meetup planer</title>
+	</head>
+	<body>
+
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+	<a class="navbar-brand" href="#">ItalianRockMafia</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+		<li class="nav-item">
+        				<a class="nav-link" href="../main.php">Home</a>
+      				</li>
+			  <li class="nav-item">
+				<a class="nav-link" href="../settings.php">Settings</a>
+			  </li>
+			  <li class="nav-item active">
+				<a class="nav-link" href="#">Meetup<span class="sr-only">(current)</span></a>
+			  </li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+				<li class="nav-item">
+        			<a class="nav-link" href="login.php?logout=1">Logout</a>
+      			</li>
+		</ul>
+	</div>
+</nav>
+<div class="topspacer"></div>
+<main role="main">
+	<div class="container">
+
+<?php
+
+require '../global/functions/apicalls.php';
+require '../global/functions/telegram.php';
+$config = require "../config.php";
+
+
+$tg_user = getTelegramUserData();
+
+if ($tg_user !== false) {
+
+} else {
+	echo '
+	<div class="alert alert-danger" role="alert">
+	<strong>Error.</strong> You need to login first
+  </div>
+';
+}
+?>
+			</div>
+		</main>
+	</body>
+</html>
