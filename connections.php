6512ce7a053ea60e709be7d0c02295b580cff34d
jhuesser <jonas.huesser+github@gmail.com>
2018-02-20 11:23:07 +0100

    connections are ready

--- connections.php
+++ connections.php
@@ -42,6 +42,8 @@ if(isset($_GET['cancel'])){
 		<script src="https://use.fontawesome.com/c414fc2c21.js"></script>
 		<title>IRM - Meetup planer</title>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 		
 
 		<script>
@@ -123,9 +125,15 @@ $response = json_decode(file_get_contents($url));
 
 if ($response->from) {
 	$from = $response->from->name;
+	$x = $response->from->coordinate->x;
+	$y = $response->from->coordinate->y;
+	$fromLink = '<a href="https://www.google.ch/maps/@' . $x .',' . $y . ',18z">';
 }
 if ($response->to) {
 	$to = $response->to->name;
+	$x = $response->to->coordinate->x;
+	$y = $response->to->coordinate->y;
+	$toLink = '<a href="https://www.google.ch/maps/@' . $x .',' . $y . ',18z">';
 }
 if (isset($response->stations->from[0])) {
 	if ($response->stations->from[0]->score < 101) {
@@ -146,25 +154,40 @@ if (isset($response->stations->to[0])) {
 	}
 }
 
-echo 'Your connection from: ' . $from . '<br>
-to: ' . $to . '<br>';
+if(isset($_GET['send'])){
+$sendText = 'Your connection from: ' .$fromLink . $from . '</a>' . chr(10). 'to: ' . $toLink . $to . '</a>' . chr(10) . 'departure: ' . date('d.m.Y H:i', strtotime($response->connections[0]->from->departure)) . chr(10) . 
+'arrival: ' . date('d.m.Y H:i', strtotime($response->connections[0]->to->arrival)) . chr(10). 'duration: ';
 
-foreach($response->connections as $connection){
-	echo '<br>';
-	echo 'departure: ' . date('d.m.Y H:i', strtotime($connection->from->departure)) . '<br>';
-	echo 'arrival: ' . date('d.m.Y H:i', strtotime($connection->to->arrival)) . '<br>duration:';
-	 echo (substr($connection->duration, 0, 2) > 0) ? htmlentities(trim(substr($connection->duration, 0, 2), '0')).'d ' : ''; 
-	echo htmlentities(trim(substr($connection->duration, 3, 1), '0').substr($connection->duration, 4, 4));?>′<br/><?php
-	//echo htmlentities(implode(', ', $connection->products));
-	foreach ($connection->sections as $section){
-		echo date('H:i', strtotime($section->departure->departure)) . ' from: ' . htmlentities($section->departure->station->name, ENT_QUOTES, 'UTF-8') . '<br>';
-		echo htmlentities($section->journey->name, ENT_QUOTES, 'UTF-8') . ' number: ' . htmlentities($section->journey->number, ENT_QUOTES, 'UTF-8') . '<br>';
-		echo date('H:i', strtotime($section->arrival->arrival)) . ' ' . htmlentities($section->arrival->station->name, ENT_QUOTES, 'UTF-8'); 
-		echo '<br>';
-	}
+$duration =  (substr($response->connections[0]->duration, 0, 2) > 0) ? htmlentities(trim(substr($response->connections[0]->duration, 0, 2), '0')).'d ' : '';
+
+$duration .= htmlentities(trim(substr($response->connections[0]->duration, 3, 1), '0').substr($response->connections[0]->duration, 4, 4)) .'′' . chr(10);
+$sendText .= $duration . chr(10) . 'journey:' . chr(10);
+
+foreach ($response->connections[0]->sections as $section){
+	$fromx = $section->departure->station->coordinate->x;
+	$fromy = $section->departure->station->coordinate->y;
+	$fromLink = '<a href="https://www.google.ch/maps/@' . $fromx .',' . $fromy . ',18z">';
+	$tox = $section->arrival->station->coordinate->x;
+	$toy = $section->arrival->station->coordinate->y;
+	$toLink = '<a href="https://www.google.ch/maps/@' . $tox .',' . $toy . ',18z">';
+	$sendText .=  date('H:i', strtotime($section->departure->departure)) . ' from ' . $fromLink .  htmlentities($section->departure->station->name, ENT_QUOTES, 'UTF-8') . '</a>' . chr(10);
+	$sendText .=  htmlentities($section->journey->name, ENT_QUOTES, 'UTF-8') . ' number: ' . htmlentities($section->journey->number, ENT_QUOTES, 'UTF-8') . chr(10);
+	$sendText .=  date('H:i', strtotime($section->arrival->arrival)) . ' to ' . $toLink . htmlentities($section->arrival->station->name, ENT_QUOTES, 'UTF-8') . '</a>' .chr(10); 
+	
+}
+$sendText .=  chr(10) . chr(10) . '<a href="https://italianrockmafia.ch/meetup/connections.php?event=' . $eventID . '">View connections on web</a>';
+
+$alertURL = "https://api.telegram.org/bot" . $config->telegram['token'] . "/sendMessage?chat_id=". $_SESSION['tgID']. "&parse_mode=HTML&text=" . urlencode($sendText);
+getCall($alertURL);
+header('Location: https://italianrockmafia.ch/meetup/event.php?event=' . $eventID);
 }
 ?>
-
+<div class="alert alert-primary alert-dismissible fade show" role="alert">
+  Click on a connection to enlarge.
+ <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
 
 
 <table class="table connections">
