cb044a310174d4a1cc4348ac45cb709310a0b6d4
jhuesser <jonas.huesser+github@gmail.com>
2018-02-22 11:20:29 +0100

    route calculation

--- route.php
+++ route.php
@@ -61,11 +61,43 @@ $event = json_decode(getCall($config->api_url . "events/" . $eventID), true);
 $startdate = strtotime($event['startdate']);
 $enddate = strtotime($event['enddate']);
 
-$result = json_decode(getCall($gmap_apiroot . "origin=" . urlencode($_SESSION['station']) . "&destination=" . urlencode($event['station']) . "&key=" . $gmapkey), true);
 
 
+$result = json_decode(getCall($gmap_apiroot . "origin=" . urlencode($_SESSION['station']) . "&destination=" . urlencode($event['station']) . "&key=" . $gmapkey . "&mode=driving&arrival_time=" . $startdate . "&units=metric&region=ch"), true);
 
-echo '<pre>'; print_r($result); echo '</pre>';
+$route = $result['routes'][0];
+
+$leg = $route['legs'][0];
+$leaveTime = $startdate - $leg['duration']['value'] - 10;
+echo '<h1>Route to: ' . $leg['end_address'] . '</h1>';
+echo '<p class="desc">Start from: ' . $leg['start_address'] . '</p>';
+echo '<p class="desc">Distance: ' . $leg['distance']['text'] . '</p>';
+echo '<p class="desc">Duration: ' . gmdate("H:i",$leg['duration']['value']) . ' h</p>';
+echo '<p class="desc">Leave at: ' . date("l, d.m.Y H:i", $leaveTime) . '</p>';
+
+
+?>
+<div id="accordion">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#routeDetail" aria-expanded="true" aria-controls="collapseOne">
+        Complete route
+        </button>
+      </h5>
+    </div>
+		<div id="routeDetail" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+<?php
+
+foreach($leg['steps'] as $step){
+	echo $step['html_instructions'] .' ' . gmdate("i",$step['duration']['value']) . ' min (' . $step['distance']['text'] . ')<br>';
+
+}?>
+      </div>
+    </div>
+  </div>
+<?php
 
 
 } else {
