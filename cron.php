b42c682d4a3f599a0df32b9fdf4d798b1a8aeb68
jhuesser <jonas.huesser+github@gmail.com>
2018-03-26 14:46:59 +0200

    fixes

--- cron.php
+++ cron.php
@@ -16,5 +16,6 @@ foreach($events['eventUsers'] as $event){
 	$alertText = urlencode('<strong>Today: /strong>' . $event['event_title']   .chr(10). 'Start: ' . date("l, d.m.Y H:i", $event['startdate'])  . chr(10) . 'Where: ' . $event['station'] . chr(10) . chr(10) . '<a href="https://italianrockmafia.ch/meetup/event.php?event=' . $event['eventID'] . '">View on web</a>' .chr(10) . '<a href="https://italianrockmafia.ch/meetup/event.php?event=' . $eventID . '&signup=1">I\'m coming, sign me up!</a>');
 	
 		$alertURL = "https://api.telegram.org/bot" . $config->telegram['token'] . "/sendMessage?chat_id=" .  $config->telegram['chatID'] . "&parse_mode=HTML&text=" . $alertText;
+		getCall($alertText);
 	}
 }
